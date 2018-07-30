f89059dfd5c22f57c7a71cd00d208022218962fe
06wj <06wj@163.com>
2018-07-30 19:15:08 +0800

    update 1.5.86

--- types/index.d.ts
+++ types/index.d.ts
@@ -571,6 +571,7 @@ declare namespace hilo3d {
      * @param node 需要朝向的元素，或者坐标
      */
     lookAt(node: Node | Object | Vector3): Node;
+    onUpdate(escapeTime: number): any;
 
     /**
      * raycast
@@ -1070,6 +1071,7 @@ declare namespace hilo3d {
      * @param node 需要朝向的元素，或者坐标
      */
     lookAt(node: Node | Object | Vector3): Node;
+    onUpdate(escapeTime: number): any;
 
     /**
      * raycast
@@ -1519,6 +1521,7 @@ declare namespace hilo3d {
      * @param node 需要朝向的元素，或者坐标
      */
     lookAt(node: Node | Object | Vector3): Node;
+    onUpdate(escapeTime: number): any;
 
     /**
      * raycast
@@ -2338,6 +2341,7 @@ declare namespace hilo3d {
      * @param node 需要朝向的元素，或者坐标
      */
     lookAt(node: Node | Object | Vector3): Node;
+    onUpdate(escapeTime: number): any;
 
     /**
      * 元素的矩阵
@@ -2720,6 +2724,7 @@ declare namespace hilo3d {
      * @param node 需要朝向的元素，或者坐标
      */
     lookAt(node: Node | Object | Vector3): Node;
+    onUpdate(escapeTime: number): any;
 
     /**
      * raycast
@@ -3040,6 +3045,7 @@ declare namespace hilo3d {
      * @param node 需要朝向的元素，或者坐标
      */
     lookAt(node: Node | Object | Vector3): Node;
+    onUpdate(escapeTime: number): any;
 
     /**
      * 元素的矩阵
@@ -3130,7 +3136,7 @@ declare namespace hilo3d {
     /**
      * 
      * @param params 创建对象的属性参数。可包含此类的所有属性，所有属性会透传给 Renderer。
-     * @param container stage的容器
+     * @param params.container stage的容器
      * @param params.canvas 指定canvas。
      * @param params.pixelRatio 像素密度。
      * @param params.clearColor 背景色。
@@ -3147,6 +3153,7 @@ declare namespace hilo3d {
     constructor(
       params?: {
         camera?: Camera;
+        container?: HTMLDivElement;
         canvas?: HTMLCanvasElement;
         width?: number;
         height?: number;
@@ -3339,7 +3346,7 @@ declare namespace hilo3d {
      * 添加一个子元素
      * @param child 需要添加的子元素
      */
-    addChild(child: Node): Node;
+    addChild(child: Node | Mesh | Light | Camera): Node;
 
     /**
      * 移除指定的子元素
@@ -3466,6 +3473,7 @@ declare namespace hilo3d {
      * @param node 需要朝向的元素，或者坐标
      */
     lookAt(node: Node | Object | Vector3): Node;
+    onUpdate(escapeTime: number): any;
 
     /**
      * raycast
@@ -3893,7 +3901,7 @@ declare namespace hilo3d {
 
   }
 
-  class BoxGeometry {
+  class BoxGeometry extends Geometry {
     /**
      * 长方体几何体
      */
@@ -4073,7 +4081,7 @@ declare namespace hilo3d {
      * 添加顶点索引
      * @param indices 顶点索引，如 addIndices(0, 1, 2)
      */
-    addIndices(...indices: number[][]): void;
+    addIndices(...indices: number[]): void;
 
     /**
      * 添加一条线
@@ -4534,7 +4542,7 @@ declare namespace hilo3d {
 
   }
 
-  class MorphGeometry {
+  class MorphGeometry extends Geometry {
     /**
      * Morph几何体
      */
@@ -4809,7 +4817,7 @@ declare namespace hilo3d {
 
   }
 
-  class PlaneGeometry {
+  class PlaneGeometry extends Geometry {
     /**
      * 平面几何体
      */
@@ -5094,7 +5102,7 @@ declare namespace hilo3d {
 
   }
 
-  class SphereGeometry {
+  class SphereGeometry extends Geometry {
     /**
      * 球形几何体
      */
@@ -5607,6 +5615,7 @@ declare namespace hilo3d {
      * @param node 需要朝向的元素，或者坐标
      */
     lookAt(node: Node | Object | Vector3): Node;
+    onUpdate(escapeTime: number): any;
 
     /**
      * raycast
@@ -5962,6 +5971,7 @@ declare namespace hilo3d {
      * @param node 需要朝向的元素，或者坐标
      */
     lookAt(node: Node | Object | Vector3): Node;
+    onUpdate(escapeTime: number): any;
 
     /**
      * 元素的矩阵
@@ -6324,6 +6334,7 @@ declare namespace hilo3d {
      * @param node 需要朝向的元素，或者坐标
      */
     lookAt(node: Node | Object | Vector3): Node;
+    onUpdate(escapeTime: number): any;
 
     /**
      * 元素的矩阵
@@ -6769,6 +6780,7 @@ declare namespace hilo3d {
      * @param node 需要朝向的元素，或者坐标
      */
     lookAt(node: Node | Object | Vector3): Node;
+    onUpdate(escapeTime: number): any;
 
     /**
      * raycast
@@ -7213,6 +7225,7 @@ declare namespace hilo3d {
      * @param node 需要朝向的元素，或者坐标
      */
     lookAt(node: Node | Object | Vector3): Node;
+    onUpdate(escapeTime: number): any;
 
     /**
      * raycast
@@ -7638,6 +7651,7 @@ declare namespace hilo3d {
      * @param node 需要朝向的元素，或者坐标
      */
     lookAt(node: Node | Object | Vector3): Node;
+    onUpdate(escapeTime: number): any;
 
     /**
      * raycast
@@ -8745,7 +8759,7 @@ declare namespace hilo3d {
 
   }
 
-  class Material {
+  class Material extends IMaterial {
     /**
      * 材质基类，一般不直接使用
      */
@@ -8755,193 +8769,13 @@ declare namespace hilo3d {
      * 
      * @param params 初始化参数，所有params都会复制到实例上
      */
-    constructor(params: Object);
+    constructor(params: IMaterial);
 
     isMaterial: boolean;
 
     className: string;
 
-    /**
-     * 光照类型
-     */
-    lightType: string;
-
-    /**
-     * 是否开启网格模式
-     */
-    wireframe: boolean;
-
-    /**
-     * 是否开启深度测试
-     */
-    depthTest: boolean;
-
-    /**
-     * SAMPLE_ALPHA_TO_COVERAGE
-     */
-    sampleAlphaToCoverage: boolean;
-
-    /**
-     * 是否开启depthMask
-     */
-    depthMask: boolean;
-
-    /**
-     * 深度测试Range
-     */
-    depthRange: any;
-
-    /**
-     * 深度测试方法
-     */
-    depthFunc: GLenum;
-
-    /**
-     * 法线贴图
-     */
-    normalMap: Texture;
-
-    /**
-     * 视差贴图
-     */
-    parallaxMap: Texture;
-
-    /**
-     * 法线贴图scale
-     */
-    normalMapScale: Vector3;
-
-    /**
-     * 是否忽略透明度
-     */
-    ignoreTranparent: boolean;
-
-    /**
-     * gammaOutput
-     */
-    gammaOutput: boolean;
-
-    /**
-     * gammaInput
-     */
-    gammaInput: boolean;
-
-    /**
-     * gamma值
-     */
-    gammaFactor: number;
-
-    /**
-     * 是否投射阴影
-     */
-    castShadows: boolean;
-
-    /**
-     * 是否接受阴影
-     */
-    receiveShadows: boolean;
-
-    /**
-     * uv transform eg:new Matrix3().fromRotationTranslationScale(Math.PI/2, 0, 0, 2, 2)
-     */
-    uvMatrix: Matrix3;
-
-    /**
-     * uv1 transform eg:new Matrix3().fromRotationTranslationScale(Math.PI/2, 0, 0, 2, 2)
-     */
-    uvMatrix1: Matrix3;
-
-    /**
-     * 是否开启 CullFace
-     */
-    cullFace: boolean;
-
-    /**
-     * CullFace 类型
-     */
-    cullFaceType: GLenum;
-
-    /**
-     * 显示面，可选值 FRONT, BACK, FRONT_AND_BACK
-     */
-    side: GLenum;
-
-    /**
-     * 是否开启颜色混合
-     */
-    blend: boolean;
-
-    /**
-     * 颜色混合方式
-     */
-    blendEquation: GLenum;
-
-    /**
-     * 透明度混合方式
-     */
-    blendEquationAlpha: GLenum;
-
-    /**
-     * 颜色混合来源比例
-     */
-    blendSrc: GLenum;
-
-    /**
-     * 颜色混合目标比例
-     */
-    blendDst: GLenum;
-
-    /**
-     * 透明度混合来源比例
-     */
-    blendSrcAlpha: GLenum;
-
-    /**
-     * 透明度混合目标比例
-     */
-    blendDstAlpha: GLenum;
-
-    /**
-     * 当前是否需要强制更新
-     */
-    isDirty: boolean;
-
-    /**
-     * 透明度 0~1
-     */
-    transparency: number;
-
-    /**
-     * 是否需要透明
-     */
-    transparent: boolean;
-
-    /**
-     * 透明度剪裁，如果渲染的颜色透明度大于等于这个值的话渲染为完全不透明，否则渲染为完全透明
-     */
-    alphaCutoff: number;
-
-    /**
-     * 是否使用HDR
-     */
-    useHDR: boolean;
-
-    /**
-     * 曝光度，仅在 useHDR 为 true 时生效
-     */
-    exposure: number;
-
     id: string;
-
-    /**
-     * 可以通过指定，semantic来指定值的获取方式，或者自定义get方法
-     */
-    uniforms: Object;
-
-    /**
-     * 可以通过指定，semantic来指定值的获取方式，或者自定义get方法
-     */
-    attributes: Object;
 
     /**
      * clone 当前Material
